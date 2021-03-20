c6bedfa3d4e965a809bc2a4d75490f4073dc6183
Tom Coakes <tomcoakes@gmail.com>
2021-03-20 15:58:29 +0000

    remove console.logs

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 import express from 'express'
 import { Express } from 'express-serve-static-core'
 import createMovieService from './src/createMovieService'
-import setupDatabase from './src/setupDatabase'
-
-import debugRouter from './src/routers/debug'
 import createDebugRouter from './src/routers/debug'
+import setupDatabase from './src/setupDatabase'
 
 setupDatabase().then((dynamoDBClient) => {
   const movieService = createMovieService(dynamoDBClient)
@@ -12,7 +10,6 @@ setupDatabase().then((dynamoDBClient) => {
 })
 
 function createAndConfigureApp(app: Express, dbService) {
-  console.log('>>> dbService: ', dbService)
   app.get('/', (req, res) => {
     console.log('>>> req: ', req.query)
     res.send('Hello, World!')
